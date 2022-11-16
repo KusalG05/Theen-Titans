e803c341d6aa3a9ec013776e90a061458d43871f
KusalG05 <krishnasaikusal@gmail.com>
2022-11-16 23:44:24 +0530

    frontend work

--- app.js
+++ app.js
@@ -16,82 +16,81 @@ app.use(express.static(__dirname+"/Profile"));
 
 //what to do when it recieves a request for the path "/" which is basically the homepath
 //req refers to the request variable and res is the response which we can modify
-// app.get('/', (req, res) => {
-//     //in response we are sending index.html file
-//     res.sendFile(__dirname + '/index.html');
-// });  
-// app.get('/account', (req, res) => {
-//     //in response we are sending index.html file
-//     res.sendFile(__dirname + '/Profile/user.html');
-// });
-// //we are listening to the port 3000 for requests
-// server.listen(3000, () => {
-//     //once we start listeing we log the message
-//     console.log('listening on port 3000')
-// });
-// app.get('/draw',(req,res)=>{
-//     res.sendFile(__dirname+"/game.html")
-// })
-// //--------------------------------setting up socket connection----------------------------------
-// player_id=0
-// word="fish";
-
-// players=[]
-
-// io.on('connection', (socket) => {
-//     const id=player_id
-//     players.push(id);
-//     socket.emit('id',player_id);
-//     player_id++;
-//     io.emit('players',players);
-//     socket.on('guess',(a)=>{
-//         if(a.guess==word){
-//             io.emit('guess',{guess:a.guess,id:id,correct:true});
-//         }
-//         else{
-//             io.emit('guess',{guess:a.guess,id:id,correct:false});
-//         }
-//     });
-//     socket.on('mouse_up',(a)=>{
-//        socket.broadcast.emit('mouse_up',a);
-//     });
-//     socket.on('mouse_move',(a)=>{
-//         socket.broadcast.emit('mouse_move',a);
-//     });
-//     socket.on('mouse_down',(a)=>{
-//         socket.broadcast.emit('mouse_down',a);
-//     });
-//     socket.on('clears',()=>{
-//         socket.broadcast.emit('clears');
-//     })
-//     socket.on('writes',()=>{
-//         socket.broadcast.emit('writes');;
-//     });
-//     socket.on('change_color',(col)=>{
-//         socket.broadcast.emit('change_color',col);
-//     });
-//     socket.on('change_width',(siz)=>{
-//         socket.broadcast.emit('change_width',siz);
-//     });
-//     socket.on('erases',()=>{
-//         socket.broadcast.emit('erases');
-//     });
-//     socket.on('disconnect', function () {
-//         players = players.filter(function (letter) {
-//             return letter !== id;
-//         });
-//         io.emit('players',players);
-//     });
-// });
-
 app.get('/', (req, res) => {
     //in response we are sending index.html file
-    res.sendFile(__dirname + '/drawing.html');
+    res.sendFile(__dirname + '/index.html');
+});  
+app.get('/account', (req, res) => {
+    //in response we are sending index.html file
+    res.sendFile(__dirname + '/Profile/user.html');
 });
 //we are listening to the port 3000 for requests
 server.listen(3000, () => {
     //once we start listeing we log the message
     console.log('listening on port 3000')
+});
+app.get('/draw',(req,res)=>{
+    res.sendFile(__dirname+"/game.html")
+})
+//--------------------------------setting up socket connection----------------------------------
+player_id=0
+word="fish";
+
+players=[]
+
+io.on('connection', (socket) => {
+    const id=player_id
+    players.push(id);
+    socket.emit('id',player_id);
+    player_id++;
+    io.emit('players',players);
+    socket.on('guess',(a)=>{
+        if(a.guess==word){
+            io.emit('guess',{guess:a.guess,id:id,correct:true});
+        }
+        else{
+            io.emit('guess',{guess:a.guess,id:id,correct:false});
+        }
+    });
+    socket.on('mouse_up',(a)=>{
+       socket.broadcast.emit('mouse_up',a);
+    });
+    socket.on('mouse_move',(a)=>{
+        socket.broadcast.emit('mouse_move',a);
+    });
+    socket.on('mouse_down',(a)=>{
+        socket.broadcast.emit('mouse_down',a);
+    });
+    socket.on('clears',()=>{
+        socket.broadcast.emit('clears');
+    })
+    socket.on('writes',()=>{
+        socket.broadcast.emit('writes');;
+    });
+    socket.on('change_color',(col)=>{
+        socket.broadcast.emit('change_color',col);
+    });
+    socket.on('change_width',(siz)=>{
+        socket.broadcast.emit('change_width',siz);
+    });
+    socket.on('erases',()=>{
+        socket.broadcast.emit('erases');
+    });
+    socket.on('disconnect', function () {
+        players = players.filter(function (letter) {
+            return letter !== id;
+        });
+        io.emit('players',players);
+    });
+});
+
+app.get('/room',(req,res)=>{
+        res.sendFile(__dirname+'/drawing.html');
+})
+
+app.get('/', (req, res) => {
+    //in response we are sending index.html file
+    res.sendFile(__dirname + '/drawing.html');
 });
 //--------------------------------setting up socket connection----------------------------------
 player_id=0
@@ -101,15 +100,32 @@ rooms=[]
 io.on('connection', (socket) => {    
     const id=player_id
     player_id++;
+    io.on('join_room',()=>{
+        if(player_id%2==0){
+            socket.join("zero");
+            zeros.push(id);
+            socket.emit('room',{room_code:0,id:player_id});    
+        }
+        else{
+            socket.join("one");
+            ones.push(id);
+            socket.emit('room',{room_code:1,id:player_id});
+        }        
+    }
+    )
+    console.log("yes");
     socket.on('create_room',()=>{
-        var room_code = Math.floor(1000 + Math.random() * 9000);
-        rooms.push(room_code);
-        socket.join(room_code);
-        socket.emit('room',{room_code:room_code,id:player_id});
+        //var room_code = Math.floor(1000 + Math.random() * 9000);
+        //rooms.push(room_code);
+        //socket.join(room_code);
+
     });
     socket.on('connect_to_room',(code)=>{
         if(rooms.includes(code)){
             socket.join(code);
+        }
+        else{
+            alert("No room to connect to");
         }
     })
 });
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { application } = require("express");
+
 function open1(){
     var open2=document.getElementById("lobby");
     let id;
@@ -10,221 +12,234 @@ function open1(){
     open2.classList.add("lobby_present");  
 }
 
-function open2(){
+function opened2(){
     var open2=document.getElementById("join");
     let id;
     open2.classList.add("lobby_present");  
 }
-// const socket=io();
-var open2=document.getElementById("lobby");
-// function open1(){
-//     open2.classList.add("lobby_present");  
-// }
+function opened(){
+    socket.emit("join_room");
+}
+function joinroom(){
+    var roomid = document.getElementById("join");
+    socket.emit("connect_to_room", roomid);
+}
+
+const socket=io();
+var opens2=document.getElementById("lobby");
+function open1(){
+    open2.classList.add("lobby_present");  
+}
 
 function close1(){
-    open2.classList.remove("lobby_present");  
+    opens2.classList.remove("lobby_present");  
     document.getElementById("room_code").innerHTML="Your room Code is: ";
 }
 function close2(){
 
-}
+};
+
+socket.on('room',(a)=>{
+    console.log(a);
+})
+
 // //--------------------------play-------------------------
-// var input = document.getElementById("myInput");
+var input = document.getElementById("myInput");
 
-// // Execute a function when the user presses a key on the keyboard
+// Execute a function when the user presses a key on the keyboard
 
-// var id;
-// var drawer=false;
+var id;
+var drawer=false;
 
-// socket.on('id',(a)=>{
-//     id=a
-//     if(id%2==0){
-//         input.addEventListener("keypress", function(event) {
-//             // If the user presses the "Enter" key on the keyboard
-//             if (event.key === "Enter") {
-//               event.preventDefault();
-//               passToServer(input.value);
-//               input.value="";
-//             }
-//         });
-//         document.getElementById("drawing_tools").innerHTML=""; 
-//     }
-//     else{
-//             canvas.addEventListener('touchstart', touchstart, false);
-//             canvas.addEventListener('touchmove', touchmove, false);
-//             canvas.addEventListener('touchend', touchend, false);        
+socket.on('id',(a)=>{
+    id=a
+    if(id%2==0){
+        input.addEventListener("keypress", function(event) {
+            // If the user presses the "Enter" key on the keyboard
+            if (event.key === "Enter") {
+              event.preventDefault();
+              passToServer(input.value);
+              input.value="";
+            }
+        });
+        document.getElementById("drawing_tools").innerHTML=""; 
+    }
+    else{
+            canvas.addEventListener('touchstart', touchstart, false);
+            canvas.addEventListener('touchmove', touchmove, false);
+            canvas.addEventListener('touchend', touchend, false);        
             
-//             canvas.addEventListener('mousedown', drawstart, false);
-//             canvas.addEventListener('mousemove', drawmove, false);
-//             canvas.addEventListener('mouseup', drawend, false);
-//             document.getElementById("guess_input").innerHTML="";   
-//     }
-//     document.getElementById("id").innerHTML="Your id is "+id
-// });
+            canvas.addEventListener('mousedown', drawstart, false);
+            canvas.addEventListener('mousemove', drawmove, false);
+            canvas.addEventListener('mouseup', drawend, false);
+            document.getElementById("guess_input").innerHTML="";   
+    }
+    document.getElementById("id").innerHTML="Your id is "+id
+});
 
-// socket.on('players',(a)=>{
-//     document.getElementById('players').innerHTML=""
-//     console.log(a);
-//     for(var i in a){
-//         console.log(a[i]);
-//         console.log(document.getElementById('players').innerHTML);
-//         if(a[i]==id){
-//             document.getElementById('players').innerHTML=document.getElementById('players').innerHTML+"<li class='list-group-item' style='width:750px; background-color:#81db76'>"+a[i]+"</li>"
-//         }
-//         else{
-//             document.getElementById('players').innerHTML=document.getElementById('players').innerHTML+"<li class='list-group-item' style='width:750px'>"+a[i]+"</li>"
-//         }
-//     }
-// })
+socket.on('players',(a)=>{
+    document.getElementById('players').innerHTML=""
+    console.log(a);
+    for(var i in a){
+        console.log(a[i]);
+        console.log(document.getElementById('players').innerHTML);
+        if(a[i]==id){
+            document.getElementById('players').innerHTML=document.getElementById('players').innerHTML+"<li class='list-group-item' style='width:750px; background-color:#81db76'>"+a[i]+"</li>"
+        }
+        else{
+            document.getElementById('players').innerHTML=document.getElementById('players').innerHTML+"<li class='list-group-item' style='width:750px'>"+a[i]+"</li>"
+        }
+    }
+})
 
-// socket.on('guess',(a)=>{
-//     let message=""
-//     if(a.id==id){
-//         message=message+"you guessed "
-//     }
-//     else{
-//         message=message+"player "+a.id+" guessed "
-//     }
-//     if(a.correct){
-//         message=message+"correct word"
-//     }
-//     else{
-//         message=message+a.guess
-//     }
-//     document.getElementById("guess").innerHTML=document.getElementById("guess").innerHTML+message+"<br>"
-// });
-// function passToServer(a){
-//     socket.emit('guess',{
-//         guess:a,
-//     });
-// };
+socket.on('guess',(a)=>{
+    let message=""
+    if(a.id==id){
+        message=message+"you guessed "
+    }
+    else{
+        message=message+"player "+a.id+" guessed "
+    }
+    if(a.correct){
+        message=message+"correct word"
+    }
+    else{
+        message=message+a.guess
+    }
+    document.getElementById("guess").innerHTML=document.getElementById("guess").innerHTML+message+"<br>"
+});
+function passToServer(a){
+    socket.emit('guess',{
+        guess:a,
+    });
+};
 
 
-// var canvas = document.getElementById('myCanvas');
-// var context = canvas.getContext('2d');
+var canvas = document.getElementById('myCanvas');
+var context = canvas.getContext('2d');
 
-// var eraser=false;
+var eraser=false;
 
-// var draw = false;
+var draw = false;
 
-// function clears(){
-//     context.clearRect(0, 0, canvas.width, canvas.height);
-//     socket.emit('clears',);
-// }
+function clears(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    socket.emit('clears',);
+}
 
-// function writes(){
-//     eraser=false;
-//     context.lineWidth=1;
-//     context.strokeStyle='#000000';
-//     socket.emit('writes',);
-// }
+function writes(){
+    eraser=false;
+    context.lineWidth=1;
+    context.strokeStyle='#000000';
+    socket.emit('writes',);
+}
 
-// function change_color(col){
-//     eraser=false;
-//     context.strokeStyle=col;
-//     socket.emit('change_color',col);
-// }
+function change_color(col){
+    eraser=false;
+    context.strokeStyle=col;
+    socket.emit('change_color',col);
+}
 
-// function change_width(siz){
-//     context.lineWidth=siz;
-//     socket.emit('change_width',siz);
-// }
+function change_width(siz){
+    context.lineWidth=siz;
+    socket.emit('change_width',siz);
+}
 
-// function erases(){
-//     eraser=true;
-//     socket.emit('erases',);
-// }
+function erases(){
+    eraser=true;
+    socket.emit('erases',);
+}
 
-// function drawstart(event) {
-//     context.beginPath();
-//     context.moveTo(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop);
-//     draw = true;
-//     socket.emit('mouse_down',{
-//         x:event.pageX,
-//         y:event.pageY
-//     });
-// }
+function drawstart(event) {
+    context.beginPath();
+    context.moveTo(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop);
+    draw = true;
+    socket.emit('mouse_down',{
+        x:event.pageX,
+        y:event.pageY
+    });
+}
 
-// function drawmove(event) {
-//     if (!draw) return;
-//     if(eraser){
-//         context.lineWidth = 40;
-//         context.strokeStyle= "#FFFFFF";
-//     }  
-//     context.lineTo(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop);
-//     context.stroke();
-//     socket.emit('mouse_move',{
-//         x:event.pageX,
-//         y:event.pageY
-//     });
-// }
+function drawmove(event) {
+    if (!draw) return;
+    if(eraser){
+        context.lineWidth = 40;
+        context.strokeStyle= "#FFFFFF";
+    }  
+    context.lineTo(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop);
+    context.stroke();
+    socket.emit('mouse_move',{
+        x:event.pageX,
+        y:event.pageY
+    });
+}
 
-// function drawend(event) {
-//     if (!draw) return;
-//     drawmove(event);
-//     draw = false;
-//     socket.emit('mouse_up',{
-//         x:event.pageX,
-//         y:event.pageY
-//     });
-// }
+function drawend(event) {
+    if (!draw) return;
+    drawmove(event);
+    draw = false;
+    socket.emit('mouse_up',{
+        x:event.pageX,
+        y:event.pageY
+    });
+}
 
-// function touchstart(event) {
-//     drawstart(event.touches[0]);
-// }
+function touchstart(event) {
+    drawstart(event.touches[0]);
+}
 
-// function touchmove(event) {
-//     event.preventDefault();
-//     drawmove(event.touches[0]);
-// }
+function touchmove(event) {
+    event.preventDefault();
+    drawmove(event.touches[0]);
+}
 
-// function touchend(event) { 
-//     drawend(event.changedTouches[0]) 
-// }
+function touchend(event) { 
+    drawend(event.changedTouches[0]) 
+}
 
-// socket.on('mouse_down',(event)=>{
-//     context.beginPath();
-//     context.moveTo(event.x - canvas.offsetLeft, event.y - canvas.offsetTop);
-//     draw = true;
-// });
-// socket.on('mouse_move',(event)=>{
-//     if (!draw) return;
-//     if(eraser){
-//         context.lineWidth = 40;
-//         context.strokeStyle= "#FFFFFF";
-//     }  
-//     context.lineTo(event.x - canvas.offsetLeft, event.y - canvas.offsetTop);
-//     context.stroke();
-// });
-// socket.on('mouse_up',(event)=>{
-//     if (!draw) return;
-//     if(eraser){
-//         context.lineWidth = 40;
-//         context.strokeStyle= "#FFFFFF";
-//     }  
-//     context.lineTo(event.x - canvas.offsetLeft, event.y - canvas.offsetTop);
-//     context.stroke();
-//     draw = false;
-// });
+socket.on('mouse_down',(event)=>{
+    context.beginPath();
+    context.moveTo(event.x - canvas.offsetLeft, event.y - canvas.offsetTop);
+    draw = true;
+});
+socket.on('mouse_move',(event)=>{
+    if (!draw) return;
+    if(eraser){
+        context.lineWidth = 40;
+        context.strokeStyle= "#FFFFFF";
+    }  
+    context.lineTo(event.x - canvas.offsetLeft, event.y - canvas.offsetTop);
+    context.stroke();
+});
+socket.on('mouse_up',(event)=>{
+    if (!draw) return;
+    if(eraser){
+        context.lineWidth = 40;
+        context.strokeStyle= "#FFFFFF";
+    }  
+    context.lineTo(event.x - canvas.offsetLeft, event.y - canvas.offsetTop);
+    context.stroke();
+    draw = false;
+});
 
-// socket.on('clears',()=>{
-//     context.beginPath();
-//     context.clearRect(0, 0, canvas.width, canvas.height); 
-// });
-// socket.on('writes',()=>{
-//     eraser=false;
-//     context.lineWidth=1;
-//     context.strokeStyle='#000000';
-// });
-// socket.on('change_color',(col)=>{
-//     eraser=false;
-//     context.strokeStyle=col;
-// });
+socket.on('clears',()=>{
+    context.beginPath();
+    context.clearRect(0, 0, canvas.width, canvas.height); 
+});
+socket.on('writes',()=>{
+    eraser=false;
+    context.lineWidth=1;
+    context.strokeStyle='#000000';
+});
+socket.on('change_color',(col)=>{
+    eraser=false;
+    context.strokeStyle=col;
+});
 
-// socket.on('change_width',(siz)=>{
-//     eraser=false;
-//     context.lineWidth=siz;
-// });
-// socket.on('erases',()=>{
-//     eraser=true;
-// });
+socket.on('change_width',(siz)=>{
+    eraser=false;
+    context.lineWidth=siz;
+});
+socket.on('erases',()=>{
+    eraser=true;
+});
